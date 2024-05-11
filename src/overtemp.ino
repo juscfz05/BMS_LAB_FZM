2b9686caff1fd24ea58291eb1815f6412814cd49
Tomasz <ichtomek@gmail.com>
2024-05-11 15:25:37 +0200

    Update variable-names
    Updated the variables as assigned by the SW-architects.

--- src/overtemp.ino
+++ src/overtemp.ino
@@ -1,6 +1,6 @@
 // Header mit Definitionen von Variablen
 
-bool overTemperatureDetected = false; // Boolescher Wert für Übertemperatur
+bool WarningOvertemp = false; // Boolescher Wert für Übertemperatur
 unsigned long previousTempMillis = 0; // Variable für Zeitstempel der letzten Temperaturmessung 
 const long tempInterval = 3000;       // Konstantes Zeitintervall in ms in dem die Temperatur gemessen werden soll (Temperaturmessung alle 3s)
 
@@ -15,15 +15,15 @@ void checkTemperature()
 
     previousTempMillis = currentMillis;                       // Setzen des Zeitstempels der neuen Messung
 
-    float cell1Temp = getCellTemp(1); // Auslesen und abspeichern Zelltemperatur 1 als cell1Temp
-    float cell2Temp = getCellTemp(2); // Auslesen und abspeichern Zelltemperatur 2 als cell2Temp
-    float cell3Temp = getCellTemp(3); // Auslesen und abspeichern Zelltemperatur 3 als cell3Temp
-    float cell4Temp = getCellTemp(4); // Auslesen und abspeichern Zelltemperatur 4 als cell4Temp
+    float Tcell1 = getCellTemp(1); // Auslesen und abspeichern Zelltemperatur 1 als cell1Temp
+    float Tcell2 = getCellTemp(2); // Auslesen und abspeichern Zelltemperatur 2 als cell2Temp
+    float Tcell3 = getCellTemp(3); // Auslesen und abspeichern Zelltemperatur 3 als cell3Temp
+    float Tcell4 = getCellTemp(4); // Auslesen und abspeichern Zelltemperatur 4 als cell4Temp
       
-    if (cell1Temp > 60 || cell2Temp > 60 || cell3Temp > 60 || cell4Temp > 60) {   // Überprüfen, ob eine der Temperaturen über 60 Grad liegt
-      overTemperatureDetected = true;   // wenn eine Zelltemperatur >60 Grad: boolesche Variable overTemperatureDetected = true
+    if (Tcell1 > 60 || Tcell2 > 60 || Tcell3 > 60 || Tcell4 > 60) {   // Überprüfen, ob eine der Temperaturen über 60 Grad liegt
+      WarningOvertemp = true;   // wenn eine Zelltemperatur >60 Grad: boolesche Variable overTemperatureDetected = true
     } else {
-      overTemperatureDetected = false;  // wenn alle Temperaturen <=60 Grad: boolesche Variable overTemperatureDetected = false
+      WarningOvertemp = false;  // wenn alle Temperaturen <=60 Grad: boolesche Variable overTemperatureDetected = false
     }
   }
 }
