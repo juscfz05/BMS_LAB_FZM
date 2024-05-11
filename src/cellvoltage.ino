0a6173276b612ec9e5ce798fde009ea78e5408b2
Rob1893 <169096056+Rob1893@users.noreply.github.com>
2024-05-11 15:21:53 +0200

    Anpassung der Variablennamen
    Hier wurde eine Anpassung der Variablennamen anhand der Vorlage der Softwarearchitekten vorgenommen.

--- src/cellvoltage.ino
+++ src/cellvoltage.ino
@@ -1,7 +1,7 @@
 // Header mit Definitionen von Variablen
 
-bool overVoltageDetected = false; // Boolescher Wert für Überspannung
-bool underVoltageDetected = false; // Boolescher Wert für Unterspannung
+bool WarningOvervoltage = false; // Boolescher Wert für Überspannung
+bool WarningUndervoltage = false; // Boolescher Wert für Unterspannung
 unsigned long previousTempMillis = 0; // Variable für Zeitstempel der letzten Spannungsmessung 
 const long tempInterval = 200;       // Konstantes Zeitintervall in ms in dem die Spannung gemessen werden soll (Spannungsmessung alle 200ms)
 
@@ -16,21 +16,21 @@ void checkcellVoltage()
 
     previousTempMillis = currentMillis;                       // Setzen des Zeitstempels der neuen Messung
 
-    float cell1Volt = getCellVoltage(1); // Auslesen und abspeichern Zellspannung 1 als cell1Volt
-    float cell2Volt = getCellVoltage(2); // Auslesen und abspeichern Zellspannung 2 als cell2Volt
-    float cell3Volt = getCellVoltage(3); // Auslesen und abspeichern Zellspannung 3 als cell3Volt
-    float cell4Volt = getCellVoltage(4); // Auslesen und abspeichern Zellspannung 4 als cell4Volt
+    float Vcell1 = getCellVoltage(1); // Auslesen und abspeichern Zellspannung 1 als Vcell1
+    float Vcell2 = getCellVoltage(2); // Auslesen und abspeichern Zellspannung 2 als Vcell2
+    float Vcell3 = getCellVoltage(3); // Auslesen und abspeichern Zellspannung 3 als Vcell3
+    float Vcell4 = getCellVoltage(4); // Auslesen und abspeichern Zellspannung 4 als Vcell4
 
-    if (cell1Volt > 4.2 || cell2Volt > 4.2 || cell3Volt > 4.2 || cell4Volt > 4.2) {   // Überprüfen, ob eine der Spannungen über 4,2 V liegt
-      overVoltageDetected = true;   // wenn eine Zellspannung > 4,2 V: boolesche Variable overVoltageDetected = true
+    if (Vcell1 > 4.2 || Vcell2 > 4.2 || Vcell3 > 4.2 || Vcell4 > 4.2) {   // Überprüfen, ob eine der Spannungen über 4,2 V liegt
+      WarningOvervoltage = true;   // wenn eine Zellspannung > 4,2 V: boolesche Variable WarningOvervoltage = true
     } else {
-      overVoltageDetected = false;  // wenn alle Spannungen <= 4,2 V: boolesche Variable overVoltageDetected = false
+      WarningOvervoltage = false;  // wenn alle Spannungen <= 4,2 V: boolesche Variable WarningOvervoltage = false
     }
-    if (cell1Volt < 2.5 || cell2Volt < 2.5 || cell3Volt < 2.5 || cell4Volt < 2.5) {   // Überprüfen, ob eine der Spannungen unter 2,5 V liegt
-      underVoltageDetected = true;   // wenn eine Zellspannung < 2,5 V: boolesche Variable underVoltageDetected = true
+    if (Vcell1 < 2.5 || Vcell2 < 2.5 || Vcell3 < 2.5 || Vcell4 < 2.5) {   // Überprüfen, ob eine der Spannungen unter 2,5 V liegt
+      WarningUndervoltage = true;   // wenn eine Zellspannung < 2,5 V: boolesche Variable WarningUndervoltage = true
     } else {
-      underVoltageDetected = false;  // wenn alle Spannungen >= 2,5 V: boolesche Variable underVoltageDetected = false
+      WarningUndervoltage = false;  // wenn alle Spannungen >= 2,5 V: boolesche Variable WarningUndervoltage = false
     }
-   
+      
   }
 }
