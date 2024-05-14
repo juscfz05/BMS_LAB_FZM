db7e98b41b8af667de744769b804e4289cd1772c
Rob1893 <169096056+Rob1893@users.noreply.github.com>
2024-05-14 18:07:02 +0200

    Anpassung des Codes
    Anpassung des Codes zum setzen der Variablen.

--- functions/cellvoltage.ino
+++ functions/cellvoltage.ino
@@ -1,15 +1,9 @@
-// Header mit Definitionen von Variablen
-
-bool WarningOvervoltage = false; // Boolescher Wert für Überspannung
-bool WarningUndervoltage = false; // Boolescher Wert für Unterspannung
-unsigned long previousVcellMillis = 0; // Variable für Zeitstempel der letzten Spannungsmessung 
-const long VcellInterval = 200;       // Konstantes Zeitintervall in ms in dem die Spannung gemessen werden soll (Spannungsmessung alle 200ms)
-
-
 // Definition der Zellspannungsüberwachungsfunktion
 
 void checkcellVoltage() 
 {
+  static unsigned long previousVcellMillis = 0;               // Variable für Zeitstempel der letzten Spannungsmessung 
+  const long VcellInterval = 200;                             // Konstantes Zeitintervall in ms in dem die Spannung gemessen werden soll (Spannungsmessung alle 200ms)
   unsigned long currentMillis = millis();                     // Variable currentMillis = aktuelle Zeit in ms 
 
   if (currentMillis - previousVcellMillis >= VcellInterval) {   // Überprüfen, ob seit letzter Messung min. 200ms vergangen sind. Falls nein, wird Funktion übersprungen.
@@ -22,15 +16,14 @@ void checkcellVoltage()
     float Vcell4 = getCellVoltage(4); // Auslesen und abspeichern Zellspannung 4 als Vcell4
 
     if (Vcell1 > 4.2 || Vcell2 > 4.2 || Vcell3 > 4.2 || Vcell4 > 4.2) {   // Überprüfen, ob eine der Spannungen über 4,2 V liegt
-      WarningOvervoltage = true;   // wenn eine Zellspannung > 4,2 V: boolesche Variable WarningOvervoltage = true
+      setWarningOvervoltage(true);   // wenn eine Zellspannung > 4,2 V, wird eine Überspannungswarnung für die VCU auf wahr gesetzt
     } else {
-      WarningOvervoltage = false;  // wenn alle Spannungen <= 4,2 V: boolesche Variable WarningOvervoltage = false
+      setWarningOvervoltage(false);  // wenn alle Spannungen <= 4,2 V, wird die Überspannungswarnung für die VCU auf falsch festgelegt 
     }
     if (Vcell1 < 2.5 || Vcell2 < 2.5 || Vcell3 < 2.5 || Vcell4 < 2.5) {   // Überprüfen, ob eine der Spannungen unter 2,5 V liegt
-      WarningUndervoltage = true;   // wenn eine Zellspannung < 2,5 V: boolesche Variable WarningUndervoltage = true
+      setWarningUndervoltage(true);   // wenn eine Zellspannung < 2,5 V, wird eine Unterspannungswarnung für die VCU auf wahr gesetzt
     } else {
-      WarningUndervoltage = false;  // wenn alle Spannungen >= 2,5 V: boolesche Variable WarningUndervoltage = false
+      setWarningUndervoltage(false);  // wenn alle Spannungen >= 2,5 V, wird die Unterspannungswarnung für die VCU auf falsch festgelegt
     }
-      
   }
 }
