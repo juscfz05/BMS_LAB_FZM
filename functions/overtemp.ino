dd4be0366c03a8b86b6371e9e83214ea9c4ca8fc
Tomasz <ichtomek@gmail.com>
2024-05-19 12:31:21 +0200

    Separate function for Tcell-Reading
    Global definition of Tcellx-variables and a separate function for reading the temperature "readTemperature()".
    
    "checkTemperature()" calls now only the "readTemperature()"-function to get the temperature-values.

--- functions/overtemp.ino
+++ functions/overtemp.ino
@@ -1,20 +1,30 @@
-// Definition der Temperaturüberwachungsfunktion
+// Globale Variablen für die Zelltemperaturen. Werden benötigt, um Variablenübergabe zwischen Funktionen zu ermöglichen.
+float Tcell1;   // Definition der globalen Variable Tcell1 für die Zelltemperatur 1
+float Tcell2;   // Definition der globalen Variable Tcell1 für die Zelltemperatur 2
+float Tcell3;   // Definition der globalen Variable Tcell1 für die Zelltemperatur 3
+float Tcell4;   // Definition der globalen Variable Tcell1 für die Zelltemperatur 4
 
+// Definition der Temperaturauslesefunktion
+void readTemperature() 
+{
+    Tcell1 = getCellTemp(1); // Auslesen und abspeichern Zelltemperatur 1 als Tcell1
+    Tcell2 = getCellTemp(2); // Auslesen und abspeichern Zelltemperatur 2 als Tcell2
+    Tcell3 = getCellTemp(3); // Auslesen und abspeichern Zelltemperatur 3 als Tcell3
+    Tcell4 = getCellTemp(4); // Auslesen und abspeichern Zelltemperatur 4 als Tcell4
+}
+
+// Definition der Temperaturüberwachungsfunktion
 void checkTemperature() 
 {
   static unsigned long previousTempMillis = 0;    // Variable für Zeitstempel der letzten Temperaturmessung. Static, dass nur bei erster Initialisierung der Wert auf 0 gesetzt werden soll. 
   const long tempInterval = 3000;                 // Konstantes Zeitintervall in ms in dem die Temperatur gemessen werden soll (Temperaturmessung alle 3s)
-
-  unsigned long currentMillis = millis();                     // Variable currentMillis = aktuelle Zeit in ms 
+  unsigned long currentMillis = millis();         // Variable currentMillis = aktuelle Zeit in ms 
 
   if (currentMillis - previousTempMillis >= tempInterval) {   // Überprüfen, ob seit letzter Messung min. 3s vergangen sind. Falls nein, wird Funktion übersprungen.
 
     previousTempMillis = currentMillis;                       // Setzen des Zeitstempels der neuen Messung
 
-    float Tcell1 = getCellTemp(1); // Auslesen und abspeichern Zelltemperatur 1 als cell1Temp
-    float Tcell2 = getCellTemp(2); // Auslesen und abspeichern Zelltemperatur 2 als cell2Temp
-    float Tcell3 = getCellTemp(3); // Auslesen und abspeichern Zelltemperatur 3 als cell3Temp
-    float Tcell4 = getCellTemp(4); // Auslesen und abspeichern Zelltemperatur 4 als cell4Temp
+    readTemperature();              // Aufruf der Temperaturauslesefunktion
       
     if (Tcell1 > 60 || Tcell2 > 60 || Tcell3 > 60 || Tcell4 > 60) {   // Überprüfen, ob eine der Temperaturen über 60 Grad liegt
       setWarningOvertemp(true);    // Setzen der Übertemperaturwarnung, dass die VCU die Fahrleistung reduziert
@@ -23,4 +33,3 @@ void checkTemperature()
     }
   }
 }
-
