d10e40804b524518d2f18a27cfb32bacba0a24fd
Tomasz <ichtomek@gmail.com>
2024-05-29 00:12:31 +0200

    Fix of overtemp bug
    Fix of a bug where the overtemp-function would give no warning for short time in the case of an overtemperature.

--- functions/overtemp.ino
+++ functions/overtemp.ino
@@ -1,23 +1,18 @@
 bool WarningOvertemp = false;                     // Globale Definition der Übertemperaturwarnung als boolesche Variable
 
-void checkTemperature(float Tcell[4])             // Definition der Temperaturüberwachungsfunktion mit der Übergabe eines Arrays mit 4 Temperaturwerten 
+void checkTemperature(float Tcell[4])              // Definition der Temperaturüberwachungsfunktion mit der Übergabe eines Arrays mit 4 Temperaturwerten 
 {
   static unsigned long previousTempMillis = 0;    // Variable für Zeitstempel der letzten Temperaturmessung. Static, dass nur bei erster Initialisierung der Wert auf 0 gesetzt werden soll. 
   const long tempInterval = 3000;                 // Konstantes Zeitintervall in ms, in dem die Temperatur gemessen werden soll (Temperaturmessung alle 3s)
   unsigned long currentMillis = millis();         // Variable currentMillis = aktuelle Zeit in ms 
+  
+  if (currentMillis - previousTempMillis >= tempInterval) {     // Überprüfen, ob seit letzter Messung mindestens 3 Sekunden vergangen sind. Falls nein, wird die Funktion übersprungen.
+    previousTempMillis = currentMillis;                         // Setzen des Zeitstempels der neuen Messung
 
-  // Überprüfen, ob seit letzter Messung mindestens 3 Sekunden vergangen sind. Falls nein, wird die Funktion übersprungen.
-  if (currentMillis - previousTempMillis >= tempInterval) {
-    previousTempMillis = currentMillis;           // Setzen des Zeitstempels der neuen Messung
-
-    WarningOvertemp = false;                      // Variable für den Rückgabewert zurücksetzen
-
-    // Schleife zur Überprüfung aller Elemente des Arrays Tcell
-    for (int i = 0; i < 4; i++) {
-      if (Tcell[i] > 60) {
-        WarningOvertemp = true;                   // Setzen der Warnung bei Übertemperatur
-        break;                                    // Schleife abbrechen, wenn Übertemperatur erkannt wurde
-      }
-    }
+    if (Tcell[0] > 60.0 || Tcell[1] > 60.0 || Tcell[2] > 60.0 || Tcell[3] > 60.0) {   // Überprüfen, ob eine der Temperaturen über 60 Grad liegt
+        WarningOvertemp = true;       // wenn eine Zelltemperatur >60 Grad: boolesche Variable WarningOvertemp = true
+    } else {
+        WarningOvertemp = false;      // wenn alle Temperaturen <=60 Grad: boolesche Variable WarningOvertemp = false
+    }   
   }
 }
